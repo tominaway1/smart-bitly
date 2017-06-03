af0acb494e9875dace9503c27d710537f69fff22
edward <edward@bytemark.co>
2017-06-03 18:13:38 -0400

    Tommy magic

--- htmlparser/views.py
+++ htmlparser/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
-
+import requests,json
 def index(request):
-    return render(request,'home/index.html')
+	print translate("hello")
+	return render(request,'home/index.html')
+
+
+def translate(text):
+	url = "http://localhost:8000/watson/translate"
+
+	payload = "{{\"src\":\"en\",\"dst\":\"fr\",\"text\":\"{0}\"}}".format(text)
+	print payload
+	headers = {}
+
+	response = requests.request("POST", url, data=payload, headers=headers)
+	response = json.loads(response.text)
+
+	return response['response']
