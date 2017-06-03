1946610ab10f68e368b8b7a56f44c37376248cf0
tommy inouye <tommyinouye@tommys-MacBook-Air.local>
2017-06-03 18:24:13 -0400

    finished home page

--- htmlparser/views.py
+++ htmlparser/views.py
@@ -1,4 +1,24 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
+from models import UrlProperties
+
 
 def index(request):
     return render(request,'home/index.html')
+
+@csrf_exempt
+def create_url(request):
+    url = None
+    if request.method == 'POST':
+        url = request.POST.get("url", "")
+
+    urlObj = UrlProperties.objects.create()
+    urlObj.url = url;
+    urlObj.save()
+    response_data = { 'url' : str(urlObj.uuid) }
+
+    print response_data
+
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
--- smartbitly/urls.py
+++ smartbitly/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', 'home.views.index', name='home'),
     url(r'^html/test$','htmlparser.views.index', name="html"),
+    url(r'^html/url$','htmlparser.views.create_url', name="html.create"),
     url(r'^watson/translate$', 'watson.views.translate',name="translate"),
     url(r'^admin/', admin.site.urls),
 ]
