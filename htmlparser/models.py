a3fa8a06be62507ef887e1eafea70a70d67dbb04
Shuyang Sun <sunbuffett@gmail.com>
2017-06-03 16:23:22 -0400

    More stuff for html model.

--- htmlparser/models.py
+++ htmlparser/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from watson.models import Language
 import uuid
 
+
 # Create your models here.
 class UrlProperties(models.Model):
     url = models.CharField(max_length=100)
@@ -12,14 +13,27 @@ class UrlProperties(models.Model):
     def __unicode__(self):
         return self.url
 
-    def _detect(self):
-        pass  # TODO: detect language from original URL
-
 
 class HtmlContent(models.Model):
     url = models.ForeignKey(UrlProperties, null=False)
     html_source = models.TextField()
     language = models.ForeignKey(Language, null=False)
+    translation_dict = {}
+
+    def __init__(self):
+        self._detect()
+        if self.language is not None:
+            self.translation_dict[self.language] = self.html_source
+
+    def _detect(self):
+        if self.language is None:
+            detected_language = None  # TODO: detect language using IBM Watson API
+            self.language = detected_language
+
+    def translate(self, lang_code):
+        translated_source = None  # TODO: translate content to another language
+        if translated_source is not None:
+            self.translation_dict[lang_code] = translated_source
 
     def __unicode__(self):
         return self.url.name + "-" + self.language
