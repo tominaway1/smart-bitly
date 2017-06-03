dd22d2d0dc1deb1815d0c7cc74d1eaa04a2356b4
tommy inouye <tommyinouye@tommys-MacBook-Air.local>
2017-06-03 14:43:36 -0400

    added new model and update home page

--- watson/models.py
+++ watson/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import uuid
 
-# Create your models here.
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    language_code = models.TextField()
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    def __unicode__(self):
+        return self.url
