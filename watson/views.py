55553af0444c481d65b9992333cd90e9b2d9b164
QED <si2281@columbia.edu>
2017-06-04 10:49:36 -0400

    Added Japanese (ja), Portuguese (pt), Italian(it), German (de),

--- watson/views.py
+++ watson/views.py
@@ -12,8 +12,20 @@ def translate(request):
         # French <-> Spanish
             "fr-es":"fr-es","es-fr":"es-fr",
         # English <-> French
-            "en-es":"en-es", "es-en":"es-en"
-        }
+            "en-es":"en-es", "es-en":"es-en",
+        # English <-> Japanese
+            "ja-en":"ja-en", "en-ja":"en-ja",
+        # English <-> Arabic, No voice support
+           # "ar-en":"ar-en", "en-ar":"en-ar",
+        # English <-> German
+            "de-en":"de-en", "en-de":"en-de",
+        # English <-> Italian
+            "it-en":"it-en", "en-it":"en-it",
+        # English <-> Portuguese
+            "pt-en":"pt-en", "en-pt":"en-pt"
+               }
+#	lang_dict={"english":"en-US_MichaelVoice", "spanish": "es-ES_EnriqueVoice","german":"de-DE_DieterVoice", "french":
+#	"fr-FR_ReneeVoice", "italian":"it-IT_FrancescaVoice","portuguese":"pt-BR_IsabelaVoice", "japanese":"ja-JP_EmiVoice"}
 
     json_data = None
     if request.method == 'POST':
@@ -33,12 +45,12 @@ def translate(request):
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 def read(request):
-    """ 
+    """
         text
         language
         html-page
 
-        Need to insert 
+        Need to insert
         <audio id = "audio" controls>
           <source id="sources" src="https://3c2fd0a4-fae3-4772-b9a1-01fce49aa56e:f5k3heyKWdJ4@stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio%2Fogg%3Bcodecs%3Dopus&voice=es-ES_EnriqueVoice&text=%20hello." type="audio/ogg">
         </audio>
